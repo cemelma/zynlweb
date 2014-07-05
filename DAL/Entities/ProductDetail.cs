547ee6d692d8bd97089f597149a6460e3ec0f2b8
miyamotomusashi <cenbek03@hotmail.com>
2014-07-05 18:06:48 +0300

    urundetay tblosu eklendi1

---
+++ DAL/Entities/ProductDetail.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class ProductDetail
+    {
+        public int DetailId{get;set;}
+        public int ProductId { get; set; }
+
+        [Display(Name = "Ürün Kodu")]
+        [Required(ErrorMessage = "Ürün Kodunu Giriniz.")]
+        public string Code { get; set; }
+
+        [Display(Name = "Malzeme")]
+        [Required(ErrorMessage = "Malzeme Adı Giriniz.")]
+        public string Material { get; set; }
+
+        [Display(Name = "Birim")]
+        [Required(ErrorMessage = "Birim Giriniz.")]
+        public string Unit { get; set; }
+
+        [Display(Name = "Ebat")]
+        [Required(ErrorMessage = "Ebat Giriniz.")]
+        public string Dimension { get; set; }
+
+        [Display(Name = "Ağırlık")]
+        [Required(ErrorMessage = "Ağırlık Giriniz.")]
+        public string Weight { get; set; }
+
+        [Display(Name = "Araç(Ton)")]
+        [Required(ErrorMessage = "Araç(Ton) Giriniz.")]
+        public string VehicleTon { get; set; }
+
+        [Display(Name = "B.Rengi TL/M2")]
+        [Required(ErrorMessage = "B.Rengi Giriniz.")]
+        public string ColorWhite { get; set; }
+
+        [Display(Name = "Kırmızı TL/M2")]
+        [Required(ErrorMessage = "Kırmızı Giriniz.")]
+        public string ColorRed { get; set; }
+    }
+}
