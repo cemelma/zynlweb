3e5deb18cfd854222a37ef3291f28493237ec985
unknown <cemelma.20@gmail.com>
2014-07-09 01:12:27 +0300

    urun

--- web/Controllers/FHomeController.cs
+++ web/Controllers/FHomeController.cs
@@ -35,7 +35,7 @@ namespace web.Controllers
             model.references = ReferenceManager.GetReferenceListForFront("tr");
 
             model.prodgroups = ProductManager.GetProductGroupList("tr").Where(x=>x.ProductGroupId!=1).Take(6);
-            int[] pgoupids = model.prodgroups.Select(d => d.TopProductId).ToArray();
+            int[] pgoupids = model.prodgroups.Select(d => d.ProductGroupId).ToArray();
             model.Products = ProductManager.GetProductTopListFront(pgoupids).ToList();
 
             return View(model);
